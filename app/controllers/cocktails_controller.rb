f3e6565fcb19d24976c20a19beceef2733df544f
Miguel Aguirre <md.aguim@gmail.com>
2018-03-29 15:56:46 -0300

    Cocktails controller done, according to specs. First attemps in Doses controller, not done yet

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,15 +1,30 @@
 class CocktailsController < ApplicationController
+
   def index
-    @cockatail = Cocktail.all
+    @cocktails = Cocktail.all
   end
 
   def show
-  end
-
-  def create
-    @cocktail = Cocktail.new
+    @cocktail = Cocktail.find(params[:id])
   end
 
   def new
+    @cocktail = Cocktail.new
   end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params) #private method
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+
 end
--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,11 +1,25 @@
 class DosesController < ApplicationController
 
-  def create
+  def new
+    @dose = Dose.new
   end
 
-  def new
+  def create
+    @dose = dose.new(dose_params) #private method
+    if @dose.save
+      redirect_to dose_path(@dose)
+    else
+      render :new
+    end
   end
 
   def destroy
   end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient)
+  end
+
 end
