11a3716f489a06aa314215e58fa5cebc29a0595d
Miguel Aguirre <md.aguim@gmail.com>
2018-03-29 16:48:19 -0300

    Controllers done and working, routes checked. Only one Cocktail intance created for testing

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,25 +1,36 @@
 class DosesController < ApplicationController
 
+before_action :set_cocktail, only: [ :new, :create ]
+
   def new
     @dose = Dose.new
   end
 
   def create
-    @dose = dose.new(dose_params) #private method
+    @dose = Dose.new(dose_params) #private method
+    @dose.cocktail = @cocktail
     if @dose.save
-      redirect_to dose_path(@dose)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
   end
 
   def destroy
+    @dose = Dose.find(params[:id])
+    @cocktail = @dose.cocktail
+    @dose.destroy
+    redirect_to cocktail_path(@cocktail)
   end
 
   private
 
   def dose_params
-    params.require(:dose).permit(:description, :ingredient)
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_cocktail
+      @cocktail = Cocktail.find(params[:cocktail_id])
   end
 
 end
